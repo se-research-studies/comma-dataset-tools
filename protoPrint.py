b8dee6206f3e4a0a9d19bf3916f1603840222bff
Julian Scholle <julian.scholle@googlemail.com>
2016-08-15 23:34:34 +0200

    add LGPL header...

--- protoPrint.py
+++ protoPrint.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python2
+# Copyright (C) 2016 Julian Scholle
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 
 import struct
