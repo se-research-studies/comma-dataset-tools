9773867db6aa61f322291f71d00be7b6d3a9174b
Julian Scholle <julian.scholle@googlemail.com>
2016-08-15 23:39:20 +0200

    LGPL -> GPL

--- protoPrint.py
+++ protoPrint.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2016 Julian Scholle
 #
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
+# modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
 #
